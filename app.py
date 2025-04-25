7004a635fbfb6b0858c5c638c11ce72fe71e1f4c
Priya-2319 <jhapriyamdb@gmail.com>
2025-04-25 23:13:42 +0530

    update in SSL

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
-app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL') == 'True'
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
 
 # File upload configuration
 UPLOAD_FOLDER = 'static/uploads/resources'
